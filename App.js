4e4d824243f2962eff6656e583bf5070588ef72a
catdog990 <ozmcastro@gmail.com>
2021-01-24 20:20:42 -0500

    customized a little

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Settings, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
 //import screens //
 import  home  from './screens/HomeScreen.js';
@@ -13,7 +14,30 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+          screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      
+      >
         <Tab.Screen name="Home" component={home} />
         <Tab.Screen name="Settings" component={settings} />
       </Tab.Navigator>
